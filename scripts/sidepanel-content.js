f9c1dc027fe2a48873b46bddafcee21c6f5449ca
schmorri <alexander.moorkamp@live.de>
2023-07-03 08:29:59 +0200

    added legal notice template & geodata attribution

---
+++ scripts/sidepanel-content.js
@@ -0,0 +1,20 @@
+let tab1 = document.getElementById("tab-1");
+
+tab1.innerHTML = `
+    <h2>Impressum</h2>
+    <p>
+        <ul>
+            <li>Autor: <a href="https://github.com/schmorri" target="_blank">schmorri</a></li>
+            <li>E-Mail: -</li>
+            <li>Telefon: -</li>
+        </ul>
+    </p>
+
+    <h2>Quellen</h2>
+    <p>
+        <ul>
+            <li>Nationalparks: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors (via Overpass API)</li>
+        </ul>   
+    </p>
+
+`
